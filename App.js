28ce0850efc92d196954469ae9e756f60bad1369
Agus Cuello <83506872+ChichiCuello@users.noreply.github.com>
2021-11-09 17:46:38 -0300

    Add files via upload
    App base, react-navigation instalado ver si todo anda correcto

---
+++ App.js
@@ -0,0 +1,21 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet, Text, View,} from 'react-native';
+
+
+
+const App = () => {
+
+
+  return (
+    <View>
+      <Text>Hola desde app clima</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
